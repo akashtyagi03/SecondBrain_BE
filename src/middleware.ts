cb49290696e6e2d52e4a523124e2719c412e8584
Aakash tyagi <akashtyagi6065@gmail.com>
2026-02-14 22:18:50 +0530

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,16 +3,23 @@ import jwt, { JwtPayload } from "jsonwebtoken";
 
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
     try {
-        const authHeader = req.headers.authorization || req.headers.Authorization;
+        const authHeader = req.headers.authorization;
+
         if (!authHeader) {
             return res.status(401).json({ error: "Authorization header missing" });
-        } else {
-            const decoded = jwt.verify(authHeader as string, typeof process.env.JWT_SECRET) as JwtPayload;
-            req.userId = decoded.userId;
-            next()
         }
-    }catch(err){
+
+        const token = authHeader.startsWith("Bearer ")
+            ? authHeader.split(" ")[1]
+            : authHeader;
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as JwtPayload;
+
+        req.userId = decoded.userId;
+        next();
+
+    } catch (err) {
         console.error('Authentication error:', err);
         return res.status(401).json({ error: "Invalid token" });
     }
-}
+};
